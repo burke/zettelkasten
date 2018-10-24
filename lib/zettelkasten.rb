ee74534bcd59d26282549ad6df5b47e5803e1fbe
Burke Libbey <burke@libbey.me>
2018-10-24 13:59:02 -0400

    fetchh

--- lib/zettelkasten.rb
+++ lib/zettelkasten.rb
@@ -4,16 +4,21 @@ require 'yaml'
 module Zettelkasten
   def self.run
     config = YAML.load_file(File.expand_path('~/.config/zettelkasten/config.yml'))
-    token = config.fetch('dynalist_api_token')
-    if md = token.match(/^keychain:(.*)$/)
-      token, _, t = Open3.capture3('security', 'find-generic-password', '-w', '-l', md[1])
-      abort 'missing dynalist-token' unless t.success?
-      token.strip!
-    end
-    note_id = config.fetch('zettelkasten_note_id')
+    token   = fetch_config(config, 'dynalist_api_token')
+    note_id = fetch_config(config, 'zettelkasten_note_id')
     dynalist = Zettelkasten::Dynalist.new(zettelkasten_note_id: note_id, dynalist_api_token: token)
 
     dynalist.run_id_maintenance
+  end
+
+  def self.fetch_config(config, key)
+    str = config.fetch(key).to_s
+    md = str.match(/^keychain:(.*)$/)
+    return str unless md
+
+    token, _, t = Open3.capture3('security', 'find-generic-password', '-w', '-l', md[1])
+    abort 'missing dynalist-token' unless t.success?
+    token.strip
   end
 end
 
