66e3b4352e2198fc3e89afd75a147ff65b16dc41
Burke Libbey <burke@libbey.me>
2018-10-24 13:56:37 -0400

    config

--- lib/zettelkasten.rb
+++ lib/zettelkasten.rb
@@ -1,11 +1,16 @@
+require 'open3'
+require 'yaml'
+
 module Zettelkasten
   def self.run
-    token = begin
-      token, _, t = Open3.capture3('security', 'find-generic-password', '-w', '-l', 'dynalist-token')
+    config = YAML.load_file(File.expand_path('~/.config/zettelkasten/config.yml'))
+    token = config.fetch('dynalist_api_token')
+    if md = token.match(/^keychain:(.*)$/)
+      token, _, t = Open3.capture3('security', 'find-generic-password', '-w', '-l', md[1])
       abort 'missing dynalist-token' unless t.success?
-      token.strip
+      token.strip!
     end
-    note_id = 'Vys54zOGRMkMlgHHZLAbRyya'
+    note_id = config.fetch('zettelkasten_note_id')
     dynalist = Zettelkasten::Dynalist.new(zettelkasten_note_id: note_id, dynalist_api_token: token)
 
     dynalist.run_id_maintenance
